5fa0a5c15e0751193a0d9b0f78d70876135d6297
di-m-dy <dimdy11.11@gmail.com>
2024-04-30 11:23:00 +0200

    add add sub_func funcs.py

--- funcs.py
+++ funcs.py
@@ -3,3 +3,10 @@ def add_func(a: int, b: int) -> int:
     :return: int, add two integer
     """
     return a + b
+
+
+def sub_func(a: int, b: int) -> int:
+    """
+    :return: int, sub two integer
+    """
+    return a - b
